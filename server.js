5415bf489b8559d66afc5e409e43372c0eb62d21
Bryan Giger <bryan@gigerbyt.es>
2019-01-08 18:23:46 +0100

    implement frontend with pokemon api

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-var express = require('express')
+const request = require('request')
+const express = require('express')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 var app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// set the view engine to ejs
+app.set('view engine', 'ejs')
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {
+
+  let urlToQuery='https://pokeapi.co/api/v2/pokemon/totodile'
+  request(urlToQuery, function (error, response, body) {
+    console.log('error:', error) // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+    data = JSON.parse(body)
+    console.log(data.name)
+    res.render('index', {data: data})
+  });
+
+})
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+})
