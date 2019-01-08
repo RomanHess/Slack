55f0dc940752bedd17b4f137b3b308663dc92f58
Bryan Giger <bryan@gigerbyt.es>
2019-01-08 18:25:53 +0100

    format

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ var app = express()
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-
-  let urlToQuery='https://pokeapi.co/api/v2/pokemon/totodile'
-  request(urlToQuery, function (error, response, body) {
+  let urlToQuery = 'https://pokeapi.co/api/v2/pokemon/totodile'
+  request(urlToQuery, function(error, response, body) {
     console.log('error:', error) // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
     data = JSON.parse(body)
     console.log(data.name)
-    res.render('index', {data: data})
-  });
-
+    res.render('index', { data: data })
+  })
 })
 
 app.listen(port, function() {
